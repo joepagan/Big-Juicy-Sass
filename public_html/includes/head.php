eef0899628340f2414481af9f76813c1ab1338df
Joe Pagan <joepagan@gmail.com>
2014-03-11 16:57:09 +0000

    removed lines from header because sometimes the no script tag contents that were there came through as the page description in google results. removed IE6, because who cares. trying to comment out chunks in the head with php to save space in the source

--- public_html/includes/head.php
+++ public_html/includes/head.php
@@ -12,20 +12,21 @@
   <link rel="stylesheet" href="<?php echo $res; ?>/css/normalize.css" />
   <link rel="stylesheet" href="<?php echo $res; ?>/css/styles.css" />
 
-  <!--[if lt IE 9]>
-    <script src="<?php echo $res; ?>/js/third_party/html5shiv/html5shiv.js"></script>
-  <![endif]-->
+<?php // Internet explorer (if you give a crap)
+  /*
+    <!--[if lt IE 9]>
+      <script src="<?php echo $res; ?>/js/third_party/html5shiv/html5shiv.js"></script>
+    <![endif]-->
 
-  <!--[if IE 8]>
-    <link rel="stylesheet" href="<?php echo $res; ?>/css/ie8.css" type="text/css" />
-  <![endif]-->
+    <!--[if IE 8]>
+      <link rel="stylesheet" href="<?php echo $res; ?>/css/ie8.css" type="text/css" />
+    <![endif]-->
 
-  <!--[if lt IE 8]>
-    <link rel="stylesheet" href="<?php echo $res; ?>/css/ie7.css" type="text/css" />
-  <![endif]-->
-
-  <!-- Magnific Popup Styles -->
-  <!--<link rel="stylesheet" type="text/css" href="<?php echo $res; ?>/js/third_party/magnific-popup/dist/magnific-popup.css" />-->  
+    <!--[if lt IE 8]>
+      <link rel="stylesheet" href="<?php echo $res; ?>/css/ie7.css" type="text/css" />
+    <![endif]-->
+  */ 
+?>
 
   <!-- Tooltipster Styles -->
   <!--<link rel="stylesheet" type="text/css" href="<?php echo $res; ?>/js/third_party/tooltipster/css/tooltipster.css" />-->
@@ -48,32 +49,41 @@
   <!-- Chosen -->
   <!--<link rel="stylesheet" type="text/css" href="<?php echo $res; ?>/js/third_party/chosen/chosen.min.css" />-->
 
-  <!-- Favicons: use http://www.xiconeditor.com to create 64/32/24/16 versions, all in one .ico
-    Or try a new service http://realfavicongenerator.net/ which creates desktop, ios, android home screen, windows 8 tiles, windows taskbar, windows surface icons too.
+<?php // Favicons
+  // use http://www.xiconeditor.com to create 64/32/24/16 versions, all in one .ico
+  // Or try a new service http://realfavicongenerator.net/ which creates desktop, ios, android home screen, windows 8 tiles, windows taskbar, windows surface icons too.
+  /*
     <link rel="icon" href="/favicon.ico" type="image/x-icon" />
     <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
-  -->
+  */
+?>
 
-  <!-- Apple icons
-  <link rel="apple-touch-icon" href="<?php echo $res; ?>/images/icons/touch-icon-iphone.png" /> 
-  <link rel="apple-touch-icon" sizes="76x76" href="<?php echo $res; ?>/images/icons/touch-icon-ipad.png" /> 
-  <link rel="apple-touch-icon" sizes="120x120" href="<?php echo $res; ?>/images/icons/touch-icon-iphone-retina.png" />
-  <link rel="apple-touch-icon" sizes="152x152" href="<?php echo $res; ?>/images/icons/touch-icon-ipad-retina.png" />
-  -->
+<?php // Apple icons
+  /*
+    <link rel="apple-touch-icon" href="<?php echo $res; ?>/images/icons/touch-icon-iphone.png" /> 
+    <link rel="apple-touch-icon" sizes="76x76" href="<?php echo $res; ?>/images/icons/touch-icon-ipad.png" /> 
+    <link rel="apple-touch-icon" sizes="120x120" href="<?php echo $res; ?>/images/icons/touch-icon-iphone-retina.png" />
+    <link rel="apple-touch-icon" sizes="152x152" href="<?php echo $res; ?>/images/icons/touch-icon-ipad-retina.png" />
+  */ 
+?>
 
-  <!-- Open Graph Protocol -->
-  <meta property="og:title" content="" />
-  <meta property="og:description" content="" />
-  <meta property="og:image" content="" />
-  <meta property="og:type" content="website" />
-  <meta property="og:site_name" content="" />
-  <meta property="og:url" content="http://www.<?php echo $_SERVER["SERVER_NAME"] . $_SERVER['REQUEST_URI']; ?>" />
-
-  <!-- Twitter Cards -->
-  <meta name="twitter:card" content="summary" />
-  <meta name="twitter:url" content="http://www.<?php echo $_SERVER["SERVER_NAME"] . $_SERVER['REQUEST_URI']; ?>" />
-  <meta name="twitter:title" content="" />
-  <meta name="twitter:description" content="" />
-  <meta name="twitter:image" content="" />
-
+<?php // Open graph protocol
+  /*
+    <meta property="og:title" content="" />
+    <meta property="og:description" content="" />
+    <meta property="og:image" content="" />
+    <meta property="og:type" content="website" />
+    <meta property="og:site_name" content="" />
+    <meta property="og:url" content="http://www.<?php echo $_SERVER["SERVER_NAME"] . $_SERVER['REQUEST_URI']; ?>" />
+  */
+?>
+<?php // Twitter Cards
+  /*
+    <meta name="twitter:card" content="summary" />
+    <meta name="twitter:url" content="http://www.<?php echo $_SERVER["SERVER_NAME"] . $_SERVER['REQUEST_URI']; ?>" />
+    <meta name="twitter:title" content="" />
+    <meta name="twitter:description" content="" />
+    <meta name="twitter:image" content="" />
+  */ 
+?>
 </head>
