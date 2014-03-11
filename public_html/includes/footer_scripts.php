9bd9e4f8e9224ef005b340b8acdddddedb14de47
Joe Pagan <joepagan@gmail.com>
2014-03-11 16:36:02 +0000

    removing unused navigation partial amended filters where a shortcut to echo didn't work.

--- public_html/includes/footer_scripts.php
+++ public_html/includes/footer_scripts.php
@@ -2,27 +2,27 @@
 
 <!-- General scripts -->
 <!--<script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.8.9/jquery-ui.min.js"></script>-->
-<!--<script src="<?php= $res; ?>/js/third_party/bootstrap/bootstrap.js"></script>-->
-<!--<script src="<?php= $res; ?>/js/third_party/tooltipster/js/jquery.tooltipster.js"></script>-->
-<!--<script src="<?php= $res; ?>/js/third_party/uniform/jquery.uniform.js"></script>-->
-<!--<script src="<?php= $res; ?>/js/third_party/easing/jquery.easing.1.3.js"></script>-->
-<!--<script src="<?php= $res; ?>/js/third_party/datejs/date.js"></script>-->
-<!--<script src="<?php= $res; ?>/js/third_party/chosen/chosen.jquery.min.js"></script>-->
-<!--<script src="<?php= $res; ?>/js/third_party/columnizer/jquery.columnizer.js"></script>-->
+<!--<script src="<?php echo $res; ?>/js/third_party/bootstrap/bootstrap.js"></script>-->
+<!--<script src="<?php echo $res; ?>/js/third_party/tooltipster/js/jquery.tooltipster.js"></script>-->
+<!--<script src="<?php echo $res; ?>/js/third_party/uniform/jquery.uniform.js"></script>-->
+<!--<script src="<?php echo $res; ?>/js/third_party/easing/jquery.easing.1.3.js"></script>-->
+<!--<script src="<?php echo $res; ?>/js/third_party/datejs/date.js"></script>-->
+<!--<script src="<?php echo $res; ?>/js/third_party/chosen/chosen.jquery.min.js"></script>-->
+<!--<script src="<?php echo $res; ?>/js/third_party/columnizer/jquery.columnizer.js"></script>-->
 
-<!--<script src="<?php= $res; ?>/js/third_party/isotope/jquery.isotope.min.js"></script>-->
+<!--<script src="<?php echo $res; ?>/js/third_party/isotope/jquery.isotope.min.js"></script>-->
 
 <!-- Lightboxes -->
-<!--<script src="<?php= $res; ?>/js/third_party/colorbox/jquery.colorbox.js"></script>-->
+<!--<script src="<?php echo $res; ?>/js/third_party/colorbox/jquery.colorbox.js"></script>-->
 
 <!-- Sliders -->
-<!--<script src="<?php= $res; ?>/js/third_party/cycle2/jquery.cycle2.js"></script>-->
-<!--<script src="<?php= $res; ?>/js/third_party/flexslider/jquery.flexslider-min.js"></script>-->
+<!--<script src="<?php echo $res; ?>/js/third_party/cycle2/jquery.cycle2.js"></script>-->
+<!--<script src="<?php echo $res; ?>/js/third_party/flexslider/jquery.flexslider-min.js"></script>-->
 
 <!-- Validate -->
-<!--<script src="<?php= $res; ?>/js/third_party/validate/jquery.validate.js"></script>-->
-<!--<script src="<?php= $res; ?>/js/third_party/validate/additional-methods.js"></script>-->
+<!--<script src="<?php echo $res; ?>/js/third_party/validate/jquery.validate.js"></script>-->
+<!--<script src="<?php echo $res; ?>/js/third_party/validate/additional-methods.js"></script>-->
 
-<!--<script src="<?php= $res; ?>/js/third_party/"></script>-->
+<!--<script src="<?php echo $res; ?>/js/third_party/"></script>-->
 
-<script src="<?php= $res; ?>/js/custom/custom.js"></script>
+<script src="<?php echo $res; ?>/js/custom/custom.js"></script>
--- public_html/includes/head.php
+++ public_html/includes/head.php
@@ -1,54 +1,52 @@
+<?php //header("X-UA-Compatible: IE=edge"); ?>
 <head>
 
   <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-  <!--<?php //header("X-UA-Compatible: IE=edge"); ?>-->
 
   <meta charset="utf-8" />
   <!-- responsive tag -->
   <!-- <meta name="viewport" content="width=device-width" /> -->
 
-  <meta name="SKYPE_TOOLBAR" content="SKYPE_TOOLBAR_PARSER_COMPATIBLE" />
-
   <title></title>
   
-  <link rel="stylesheet" href="<?php= $res; ?>/css/normalize.css" />
-  <link rel="stylesheet" href="<?php= $res; ?>/css/styles.css" />
+  <link rel="stylesheet" href="<?php echo $res; ?>/css/normalize.css" />
+  <link rel="stylesheet" href="<?php echo $res; ?>/css/styles.css" />
 
   <!--[if lt IE 9]>
-    <script src="<?php= $res; ?>/js/third_party/html5shiv/html5shiv.js"></script>
+    <script src="<?php echo $res; ?>/js/third_party/html5shiv/html5shiv.js"></script>
   <![endif]-->
 
   <!--[if IE 8]>
-    <link rel="stylesheet" href="<?php= $res; ?>/css/ie8.css" type="text/css" />
+    <link rel="stylesheet" href="<?php echo $res; ?>/css/ie8.css" type="text/css" />
   <![endif]-->
 
   <!--[if lt IE 8]>
-    <link rel="stylesheet" href="<?php= $res; ?>/css/ie7.css" type="text/css" />
+    <link rel="stylesheet" href="<?php echo $res; ?>/css/ie7.css" type="text/css" />
   <![endif]-->
 
   <!-- Magnific Popup Styles -->
-  <!--<link rel="stylesheet" type="text/css" href="<?php= $res; ?>/js/third_party/magnific-popup/dist/magnific-popup.css" />-->  
+  <!--<link rel="stylesheet" type="text/css" href="<?php echo $res; ?>/js/third_party/magnific-popup/dist/magnific-popup.css" />-->  
 
   <!-- Tooltipster Styles -->
-  <!--<link rel="stylesheet" type="text/css" href="<?php= $res; ?>/js/third_party/tooltipster/css/tooltipster.css" />-->
+  <!--<link rel="stylesheet" type="text/css" href="<?php echo $res; ?>/js/third_party/tooltipster/css/tooltipster.css" />-->
 
   <!-- Colorbox Styles -->
   <!-- 
-      <link rel="stylesheet" type="text/css" href="<?php= $res; ?>/js/third_party/colorbox/example1/colorbox.css" />
-      <link rel="stylesheet" type="text/css" href="<?php= $res; ?>/js/third_party/colorbox/example2/colorbox.css" />
-      <link rel="stylesheet" type="text/css" href="<?php= $res; ?>/js/third_party/colorbox/example3/colorbox.css" />
-      <link rel="stylesheet" type="text/css" href="<?php= $res; ?>/js/third_party/colorbox/example4/colorbox.css" />
-      <link rel="stylesheet" type="text/css" href="<?php= $res; ?>/js/third_party/colorbox/example5/colorbox.css" />
+      <link rel="stylesheet" type="text/css" href="<?php echo $res; ?>/js/third_party/colorbox/example1/colorbox.css" />
+      <link rel="stylesheet" type="text/css" href="<?php echo $res; ?>/js/third_party/colorbox/example2/colorbox.css" />
+      <link rel="stylesheet" type="text/css" href="<?php echo $res; ?>/js/third_party/colorbox/example3/colorbox.css" />
+      <link rel="stylesheet" type="text/css" href="<?php echo $res; ?>/js/third_party/colorbox/example4/colorbox.css" />
+      <link rel="stylesheet" type="text/css" href="<?php echo $res; ?>/js/third_party/colorbox/example5/colorbox.css" />
   -->
 
   <!-- Isotope -->
-  <!--<link rel="stylesheet" type="text/css" href="<?php= $res; ?>/js/third_party/isotope/isotope.css" />-->
+  <!--<link rel="stylesheet" type="text/css" href="<?php echo $res; ?>/js/third_party/isotope/isotope.css" />-->
 
   <!-- Flex Slider -->
-  <!--<link rel="stylesheet" type="text/css" href="<?php= $res; ?>/js/third_party/flexslider/flexslider.css" />-->
+  <!--<link rel="stylesheet" type="text/css" href="<?php echo $res; ?>/js/third_party/flexslider/flexslider.css" />-->
 
   <!-- Chosen -->
-  <!--<link rel="stylesheet" type="text/css" href="<?php= $res; ?>/js/third_party/chosen/chosen.min.css" />-->
+  <!--<link rel="stylesheet" type="text/css" href="<?php echo $res; ?>/js/third_party/chosen/chosen.min.css" />-->
 
   <!-- Favicons: use http://www.xiconeditor.com to create 64/32/24/16 versions, all in one .ico
     Or try a new service http://realfavicongenerator.net/ which creates desktop, ios, android home screen, windows 8 tiles, windows taskbar, windows surface icons too.
@@ -57,10 +55,10 @@
   -->
 
   <!-- Apple icons
-  <link rel="apple-touch-icon" href="<?php= $res; ?>/images/icons/touch-icon-iphone.png" /> 
-  <link rel="apple-touch-icon" sizes="76x76" href="<?php= $res; ?>/images/icons/touch-icon-ipad.png" /> 
-  <link rel="apple-touch-icon" sizes="120x120" href="<?php= $res; ?>/images/icons/touch-icon-iphone-retina.png" />
-  <link rel="apple-touch-icon" sizes="152x152" href="<?php= $res; ?>/images/icons/touch-icon-ipad-retina.png" />
+  <link rel="apple-touch-icon" href="<?php echo $res; ?>/images/icons/touch-icon-iphone.png" /> 
+  <link rel="apple-touch-icon" sizes="76x76" href="<?php echo $res; ?>/images/icons/touch-icon-ipad.png" /> 
+  <link rel="apple-touch-icon" sizes="120x120" href="<?php echo $res; ?>/images/icons/touch-icon-iphone-retina.png" />
+  <link rel="apple-touch-icon" sizes="152x152" href="<?php echo $res; ?>/images/icons/touch-icon-ipad-retina.png" />
   -->
 
   <!-- Open Graph Protocol -->
@@ -69,11 +67,11 @@
   <meta property="og:image" content="" />
   <meta property="og:type" content="website" />
   <meta property="og:site_name" content="" />
-  <meta property="og:url" content="http://www.<?php= $_SERVER["SERVER_NAME"] . $_SERVER['REQUEST_URI']; ?>" />
+  <meta property="og:url" content="http://www.<?php echo $_SERVER["SERVER_NAME"] . $_SERVER['REQUEST_URI']; ?>" />
 
   <!-- Twitter Cards -->
   <meta name="twitter:card" content="summary" />
-  <meta name="twitter:url" content="http://www.<?php= $_SERVER["SERVER_NAME"] . $_SERVER['REQUEST_URI']; ?>" />
+  <meta name="twitter:url" content="http://www.<?php echo $_SERVER["SERVER_NAME"] . $_SERVER['REQUEST_URI']; ?>" />
   <meta name="twitter:title" content="" />
   <meta name="twitter:description" content="" />
   <meta name="twitter:image" content="" />
