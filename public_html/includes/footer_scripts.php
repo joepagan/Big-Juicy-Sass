ead080924a5b2210c1fd6e756d067da1aa8dacb3
Joe Pagan <joepagan@gmail.com>
2014-03-18 09:14:51 +0000

    moved jquery migrate to separate folder added can fallback commented out scripts & head files with php instead of html, now it hides it from the markup if you want to leave them in for whatever reason.

--- public_html/includes/footer_scripts.php
+++ public_html/includes/footer_scripts.php
@@ -1,26 +1,32 @@
 <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
-<script src="//code.jquery.com/jquery-migrate-1.2.1.js"></script>
-<!-- General scripts -->
-<!--<script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.8.9/jquery-ui.min.js"></script>-->
-<!--<script src="<?php echo $res; ?>/js/third_party/bootstrap/bootstrap.js"></script>-->
-<!--<script src="<?php echo $res; ?>/js/third_party/tooltipster/js/jquery.tooltipster.js"></script>-->
-<!--<script src="<?php echo $res; ?>/js/third_party/uniform/jquery.uniform.js"></script>-->
-<!--<script src="<?php echo $res; ?>/js/third_party/easing/jquery.easing.1.3.js"></script>-->
-<!--<script src="<?php echo $res; ?>/js/third_party/datejs/date.js"></script>-->
-<!--<script src="<?php echo $res; ?>/js/third_party/chosen/chosen.jquery.min.js"></script>-->
-<!--<script src="<?php echo $res; ?>/js/third_party/columnizer/jquery.columnizer.js"></script>-->
-<!--<script src="<?php echo $res; ?>/js/third_party/validate/jquery.validate.js"></script>-->
-<!--<script src="<?php echo $res; ?>/js/third_party/waypoints/waypoints.js"></script>-->
-<!--<script src="<?php echo $res; ?>/js/third_party/isotope/jquery.isotope.min.js"></script>-->
-<!--<script src="<?php echo $res; ?>/js/third_party/isotope/jquery.mixitup.min.js"></script>-->
+<script>
+	// CDN Fallback
+	if (typeof jQuery == 'undefined') {
+	    document.write(unescape("%3Cscript src='<?php echo $res; ?>/js/third_party/jquery/jquery-1.11.0.min.js' type='text/javascript'%3E%3C/script%3E"));
+	}	
+</script>
+<script src="<?php echo $res; ?>/js/third_party/jquery-migrate/jquery-migrate-1.2.1.min.js"></script>
+<? //<!-- General scripts --> ?>
+<? /*<script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.8.9/jquery-ui.min.js"></script> */ ?>
+<? /*<script src="<?php echo $res; ?>/js/third_party/bootstrap/bootstrap.js"></script> */ ?>
+<? /*<script src="<?php echo $res; ?>/js/third_party/tooltipster/js/jquery.tooltipster.js"></script> */ ?>
+<? /*<script src="<?php echo $res; ?>/js/third_party/uniform/jquery.uniform.js"></script> */ ?>
+<? /*<script src="<?php echo $res; ?>/js/third_party/easing/jquery.easing.1.3.js"></script> */ ?>
+<? /*<script src="<?php echo $res; ?>/js/third_party/datejs/date.js"></script> */ ?>
+<? /*<script src="<?php echo $res; ?>/js/third_party/chosen/chosen.jquery.min.js"></script> */ ?>
+<? /*<script src="<?php echo $res; ?>/js/third_party/columnizer/jquery.columnizer.js"></script> */ ?>
+<? /*<script src="<?php echo $res; ?>/js/third_party/validate/jquery.validate.js"></script> */ ?>
+<? /*<script src="<?php echo $res; ?>/js/third_party/waypoints/waypoints.js"></script> */ ?>
+<? /*<script src="<?php echo $res; ?>/js/third_party/isotope/jquery.isotope.min.js"></script> */ ?>
+<? /*<script src="<?php echo $res; ?>/js/third_party/isotope/jquery.mixitup.min.js"></script> */ ?>
 
-<!-- Lightboxes -->
-<!--<script src="<?php echo $res; ?>/js/third_party/colorbox/jquery.colorbox.js"></script>-->
+<? /* Lightboxes  */ ?>
+<? /*<script src="<?php echo $res; ?>/js/third_party/colorbox/jquery.colorbox.js"></script> */ ?>
 
-<!-- Sliders -->
-<!--<script src="<?php echo $res; ?>/js/third_party/cycle2/jquery.cycle2.js"></script>-->
-<!--<script src="<?php echo $res; ?>/js/third_party/flexslider/jquery.flexslider-min.js"></script>-->
+<? /* Sliders  */ ?>
+<? /*<script src="<?php echo $res; ?>/js/third_party/cycle2/jquery.cycle2.js"></script> */ ?>
+<? /*<script src="<?php echo $res; ?>/js/third_party/flexslider/jquery.flexslider-min.js"></script> */ ?>
 
-<!--<script src="<?php echo $res; ?>/js/third_party/"></script>-->
+<? /*<script src="<?php echo $res; ?>/js/third_party/"></script> */ ?>
 
 <script src="<?php echo $res; ?>/js/custom/custom.js"></script>
--- public_html/includes/head.php
+++ public_html/includes/head.php
@@ -4,63 +4,59 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge" />
 
   <meta charset="utf-8" />
-  <!-- responsive tag -->
-  <!-- <meta name="viewport" content="width=device-width" /> -->
+  <? /* responsive tag  */ ?>
+  <? /* <meta name="viewport" content="width=device-width" /> */ ?>
 
   <title></title>
   
   <link rel="stylesheet" href="<?php echo $res; ?>/css/normalize.css" />
   <link rel="stylesheet" href="<?php echo $res; ?>/css/styles.css" />
 
-<?php // Internet explorer (if you give a crap)
-  /*
-    <!--[if lt IE 9]>
-      <script src="<?php echo $res; ?>/js/third_party/html5shiv/html5shiv.min.js"></script>
-      <script src="<?php echo $res; ?>/js/third_party/html5shiv/html5shiv-printshiv.min.js"></script>
-    <![endif]-->
+<?php // Internet explorer (if you give a care) ?>
+  <!--[if lt IE 9]>
+    <script src="<?php echo $res; ?>/js/third_party/html5shiv/html5shiv.min.js"></script>
+    <script src="<?php echo $res; ?>/js/third_party/html5shiv/html5shiv-printshiv.min.js"></script>
+  <![endif]-->
 
-    <!--[if IE 8]>
+<? /*
+    <!--[if lte IE 8]>
       <link rel="stylesheet" href="<?php echo $res; ?>/css/ie8.css" type="text/css" />
     <![endif]-->
 
-    <!--[if lt IE 8]>
+    <!--[if lte IE 7]>
       <link rel="stylesheet" href="<?php echo $res; ?>/css/ie7.css" type="text/css" />
     <![endif]-->
   */ 
-?>
+?>  
 
-  <!-- Tooltipster Styles -->
-  <!--<link rel="stylesheet" type="text/css" href="<?php echo $res; ?>/js/third_party/tooltipster/css/tooltipster.css" />-->
+  <? /* Colorbox Styles  */ ?>
+  <? /* <link rel="stylesheet" type="text/css" href="<?php echo $res; ?>/js/third_party/colorbox/example1/colorbox.css" /> */ ?>
+  <? /* <link rel="stylesheet" type="text/css" href="<?php echo $res; ?>/js/third_party/colorbox/example2/colorbox.css" /> */ ?>
+  <? /* <link rel="stylesheet" type="text/css" href="<?php echo $res; ?>/js/third_party/colorbox/example3/colorbox.css" /> */ ?>
+  <? /* <link rel="stylesheet" type="text/css" href="<?php echo $res; ?>/js/third_party/colorbox/example4/colorbox.css" /> */ ?>
+  <? /* <link rel="stylesheet" type="text/css" href="<?php echo $res; ?>/js/third_party/colorbox/example5/colorbox.css" /> */ ?>
+  
 
-  <!-- Colorbox Styles -->
-  <!-- 
-      <link rel="stylesheet" type="text/css" href="<?php echo $res; ?>/js/third_party/colorbox/example1/colorbox.css" />
-      <link rel="stylesheet" type="text/css" href="<?php echo $res; ?>/js/third_party/colorbox/example2/colorbox.css" />
-      <link rel="stylesheet" type="text/css" href="<?php echo $res; ?>/js/third_party/colorbox/example3/colorbox.css" />
-      <link rel="stylesheet" type="text/css" href="<?php echo $res; ?>/js/third_party/colorbox/example4/colorbox.css" />
-      <link rel="stylesheet" type="text/css" href="<?php echo $res; ?>/js/third_party/colorbox/example5/colorbox.css" />
-  -->
+  <? /*  Isotope  */ ?>
+  <? /* <link rel="stylesheet" type="text/css" href="<?php echo $res; ?>/js/third_party/isotope/isotope.css" /> */ ?>
 
-  <!-- Isotope -->
-  <!--<link rel="stylesheet" type="text/css" href="<?php echo $res; ?>/js/third_party/isotope/isotope.css" />-->
+  <? /*  Flex Slider  */ ?>
+  <? /* <link rel="stylesheet" type="text/css" href="<?php echo $res; ?>/js/third_party/flexslider/flexslider.css" /> */ ?>
 
-  <!-- Flex Slider -->
-  <!--<link rel="stylesheet" type="text/css" href="<?php echo $res; ?>/js/third_party/flexslider/flexslider.css" />-->
+  <? /*  Chosen  */ ?>
+  <? /* <link rel="stylesheet" type="text/css" href="<?php echo $res; ?>/js/third_party/chosen/chosen.min.css" /> */ ?>
 
-  <!-- Chosen -->
-  <!--<link rel="stylesheet" type="text/css" href="<?php echo $res; ?>/js/third_party/chosen/chosen.min.css" />-->
-
-<?php // Favicons
-  // use http://www.xiconeditor.com to create 64/32/24/16 versions, all in one .ico
-  // Or try a new service http://realfavicongenerator.net/ which creates desktop, ios, android home screen, windows 8 tiles, windows taskbar, windows surface icons too.
+<?php /* Favicons 
+  use http://www.xiconeditor.com to create 64/32/24/16 versions, all in one .ico
+  Or try a new service http://realfavicongenerator.net/ which creates desktop, ios, android home screen, windows 8 tiles, windows taskbar, windows surface icons too.
+  */
   /*
     <link rel="icon" href="/favicon.ico" type="image/x-icon" />
     <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
   */
 ?>
 
-<?php // Apple icons
-  /*
+<?php /* Apple icons 
     <link rel="apple-touch-icon" href="<?php echo $res; ?>/images/icons/touch-icon-iphone.png" /> 
     <link rel="apple-touch-icon" sizes="76x76" href="<?php echo $res; ?>/images/icons/touch-icon-ipad.png" /> 
     <link rel="apple-touch-icon" sizes="120x120" href="<?php echo $res; ?>/images/icons/touch-icon-iphone-retina.png" />
