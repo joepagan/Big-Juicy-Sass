991494c82e6c20d916f77e98b52eba0de732e317
Joe Pagan <joepagan@gmail.com>
2014-03-29 18:42:16 +0000

    added comment mentioning you have to pay for 2 plugins, moved chosen next to uniform in markup as they're sort of similar

--- public_html/includes/footer_scripts.php
+++ public_html/includes/footer_scripts.php
@@ -11,14 +11,12 @@
 <? /*<script src="<?php echo $res; ?>/js/third_party/bootstrap/bootstrap.js"></script> */ ?>
 <? /*<script src="<?php echo $res; ?>/js/third_party/tooltipster/js/jquery.tooltipster.js"></script> */ ?>
 <? /*<script src="<?php echo $res; ?>/js/third_party/uniform/jquery.uniform.js"></script> */ ?>
+<? /*<script src="<?php echo $res; ?>/js/third_party/chosen/chosen.jquery.min.js"></script> */ ?>
 <? /*<script src="<?php echo $res; ?>/js/third_party/easing/jquery.easing.1.3.js"></script> */ ?>
 <? /*<script src="<?php echo $res; ?>/js/third_party/datejs/date.js"></script> */ ?>
-<? /*<script src="<?php echo $res; ?>/js/third_party/chosen/chosen.jquery.min.js"></script> */ ?>
 <? /*<script src="<?php echo $res; ?>/js/third_party/columnizer/jquery.columnizer.js"></script> */ ?>
 <? /*<script src="<?php echo $res; ?>/js/third_party/validate/jquery.validate.min.js"></script> */ ?>
 <? /*<script src="<?php echo $res; ?>/js/third_party/waypoints/waypoints.min.js"></script> */ ?>
-<? /*<script src="<?php echo $res; ?>/js/third_party/isotope/jquery.isotope.min.js"></script> */ ?>
-<? /*<script src="<?php echo $res; ?>/js/third_party/mixitup/jquery.mixitup.min.js"></script> */ ?>
 
 <? /* Lightboxes  */ ?>
 <? /*<script src="<?php echo $res; ?>/js/third_party/magnific/jquery.magnific-popup.min.js"></script> */ ?>
@@ -27,6 +25,10 @@
 <? /* Sliders  */ ?>
 <? /*<script src="<?php echo $res; ?>/js/third_party/cycle2/jquery.cycle2.js"></script> */ ?>
 <? /*<script src="<?php echo $res; ?>/js/third_party/flexslider/jquery.flexslider-min.js"></script> */ ?>
+
+<? /* You'll have to pay for a license to use the following */ ?>
+<? /*<script src="<?php echo $res; ?>/js/third_party/isotope/jquery.isotope.min.js"></script> */ ?>
+<? /*<script src="<?php echo $res; ?>/js/third_party/mixitup/jquery.mixitup.min.js"></script> */ ?>
 
 <? /* If you want to add any others to your project easy just duplicate the line below */ ?>
 <? /*<script src="<?php echo $res; ?>/js/third_party/"></script> */ ?>
