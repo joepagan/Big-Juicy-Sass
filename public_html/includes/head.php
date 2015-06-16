224be607db7cd76f0c84ff10bbe96f4dab86e60c
Joe Pagan <joe.pagan@extreme-creations.co.uk>
2015-06-16 12:24:12 +0100

    adding improved viewport meta tag

--- public_html/includes/head.php
+++ public_html/includes/head.php
@@ -3,8 +3,7 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
 
   <meta charset="utf-8">
-  <?php /* responsive tag  */ ?>
-  <?php /* <meta name="viewport" content="width=device-width" /> */ ?>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
   <title>Big Juicy SASS</title>
 
